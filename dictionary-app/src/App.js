1d94e53587818f471a48347501bda5a1f7a7eef1
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-10-10 08:02:01 -0400

    Initial header component structure

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import '../dist/output.css';
+// import '../dist/output.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
--- dictionary-app/src/components/Header.js
+++ dictionary-app/src/components/Header.js
@@ -1,14 +1,114 @@
 function Header() {
-    return (
-        <>
-        <header>
-            <div className="header">
-                <h1>Dictionary Web App</h1>
+  return (
+    <>
+      <div className="header-container">
+        <svg
+          className="header-logo"
+          xmlns="http://www.w3.org/2000/svg"
+          width="34"
+          height="38"
+          viewBox="0 0 34 38"
+        >
+          <g
+            fill="none"
+            fill-rule="evenodd"
+            stroke="#838383"
+            stroke-linecap="round"
+            stroke-width="1.5"
+          >
+            <path d="M1 33V5a4 4 0 0 1 4-4h26.8A1.2 1.2 0 0 1 33 2.2v26.228M5 29h28M5 37h28" />
+            <path stroke-linejoin="round" d="M5 37a4 4 0 1 1 0-8" />
+            <path d="M11 9h12" />
+          </g>
+        </svg>
+        <div className="font-dropdown">
+          <div class="relative inline-block text-left">
+            <div>
+              <button
+                type="button"
+                class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                id="menu-button"
+                aria-expanded="true"
+                aria-haspopup="true"
+              >
+                Font Selected
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="8" viewBox="0 0 14 8"><path fill="none" stroke="#A445ED" stroke-width="1.5" d="m1 1 6 6 6-6"/></svg>
+              </button>
             </div>
-        </header>
-        </>
-    )
 
+            <div
+              class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+              tabindex="-1"
+            >
+              <div class="py-1" role="none">
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-0"
+                >
+                  Account settings
+                </a>
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-1"
+                >
+                  Support
+                </a>
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-2"
+                >
+                  License
+                </a>
+                <form method="POST" action="#" role="none">
+                  <button
+                    type="submit"
+                    class="text-gray-700 block w-full px-4 py-2 text-left text-sm"
+                    role="menuitem"
+                    tabindex="-1"
+                    id="menu-item-3"
+                  >
+                    Sign out
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mode-toggle-container">
+          <div className="mode-toggle">
+            <svg
+              className="moon"
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              viewBox="0 0 22 22"
+            >
+              <path
+                fill="none"
+                stroke="#838383"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Header;
