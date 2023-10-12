34b9e59db1183ad6e5dd303f94246076c030d20c
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-10-12 13:59:26 -0400

    Update dropdown display logic for DropdownMenu component

--- dictionary-app/src/components/Header.js
+++ dictionary-app/src/components/Header.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 import DropdownMenu from "./DropdownMenu";
 
 function Header() {
-//   const button = document.getElementById("menu-button");
-//   const menu = document.querySelector("[role='menu']");
+  //   const button = document.getElementById("menu-button");
+  //   const menu = document.querySelector("[role='menu']");
 
-//   button.addEventListener("click", () => {
-//     menu.ClassList.toggle("hidden");
-//   });
-  
+  //   button.addEventListener("click", () => {
+  //     menu.ClassList.toggle("hidden");
+  //   });
 
-
-    return (
+  return (
     <>
       <div className="header-container">
         <svg
@@ -34,69 +32,7 @@ function Header() {
           </g>
         </svg>
         <div className="font-dropdown">
-          <div class="relative inline-block text-left">
-            <div>
-              <button
-                type="button"
-                class="header-btn inline-flex w-full justify-center gap-x-1.5 rounded-md bg-none px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                id="menu-button"
-                aria-expanded="true"
-                aria-haspopup="true"
-              >
-                Font Selected
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="8" viewBox="0 0 14 8"><path fill="none" stroke="#A445ED" stroke-width="1.5" d="m1 1 6 6 6-6"/></svg>
-              </button>
-            </div>
-
-            <div
-              class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="menu-button"
-              tabindex="-1"
-            >
-              <div class="py-1" role="none">
-                <a
-                  href="#"
-                  class="text-gray-700 block px-4 py-2 text-sm"
-                  role="menuitem"
-                  tabindex="-1"
-                  id="menu-item-0"
-                >
-                  Account settings
-                </a>
-                <a
-                  href="#"
-                  class="text-gray-700 block px-4 py-2 text-sm"
-                  role="menuitem"
-                  tabindex="-1"
-                  id="menu-item-1"
-                >
-                  Support
-                </a>
-                <a
-                  href="#"
-                  class="text-gray-700 block px-4 py-2 text-sm"
-                  role="menuitem"
-                  tabindex="-1"
-                  id="menu-item-2"
-                >
-                  License
-                </a>
-                <form method="POST" action="#" role="none">
-                  <button
-                    type="submit"
-                    class="text-gray-700 block w-full px-4 py-2 text-left text-sm"
-                    role="menuitem"
-                    tabindex="-1"
-                    id="menu-item-3"
-                  >
-                    Sign out
-                  </button>
-                </form>
-              </div>
-            </div>
-          </div>
+          <DropdownMenu />
         </div>
         <div className="mode-toggle-container">
           <div className="mode-toggle">
