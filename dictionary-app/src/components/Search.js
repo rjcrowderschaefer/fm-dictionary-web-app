a4d492a9c581165d5a8ff954909d2540f32fea22
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-10-16 18:38:57 -0400

    Add error message logic for empty input

--- dictionary-app/src/components/Search.js
+++ dictionary-app/src/components/Search.js
@@ -1,22 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-function Search( {onSearch} ) {
+function Search({ onSearch }) {
   const [searchedWord, setSearchedWord] = useState("");
+  const errorRef = useRef(null);
+  const inputRef = useRef(null);
 
-  
+
+useEffect(() => {
+    inputRef.current.style.border = "1px solid transparent";
+}, []);
+
   const handleChange = (e) => {
     setSearchedWord(e.target.value);
+    errorRef.current.style.display = "none";
+    inputRef.current.style.border = "1px solid transparent";
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSearch(searchedWord);
+    if (validate()) {
+      onSearch(searchedWord);
+    }
   };
+  
+  function validate() {
+    const word = searchedWord;
+    const wordLength = word.length;
+    console.log(word);
+    if (wordLength <= 0) {
+        errorRef.current.style.display = "block";
+        inputRef.current.style.border = "1px solid #FF5252"
+        return false;
+    }
+    return true;
+}
 
   return (
     <>
       <div className="search-container">
-        <form onSubmit={handleSubmit}>
+        <form 
+        ref={inputRef}
+        onSubmit={handleSubmit}>
           <label htmlFor="word"></label>
           <input
             type="search"
@@ -28,6 +52,9 @@ function Search( {onSearch} ) {
           />
           <button type="submit" id="search-button"></button>
         </form>
+      </div>
+      <div id="required-error" className="empty-error" ref={errorRef}>
+        Whoops, can't be empty...
       </div>
     </>
   );
