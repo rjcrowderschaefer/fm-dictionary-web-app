2ae9cd6734a0405330736556046d7bbfd18a841c
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-10-17 13:22:25 -0400

    Add layout stying to results meaning header for mobile

--- dictionary-app/src/components/Results.js
+++ dictionary-app/src/components/Results.js
@@ -19,68 +19,71 @@ function Results({ errorMessage, results }) {
           <h1>{result.word}</h1>
           <h2>{result.phonetic}</h2>
           <div className="audio">
-          {result.phonetics &&
-            result.phonetics.map((phonetic, phoneticIdx) => (
-              <div key={phoneticIdx}>
-                {phonetic.audio &&
-                  phonetic.audio.trim() !== "" &&
-                  (phoneticIdx === 0 ||
-                    !result.phonetics
-                      .slice(0, phoneticIdx)
-                      .some((p) => p.audio.trim() !== "")) && (
-                    <div>
-                      <audio id={`audio_${idx}_${phoneticIdx}`}>
-                        <source src={phonetic.audio} type="audio/mpeg" />
-                      </audio>
-                      <button
-                        id={`playButton_${idx}_${phoneticIdx}`}
-                        onClick={() => handlePlayClick(idx, phoneticIdx)}
-                      >
-                        <svg
-                          className="playSVG"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="75"
-                          height="75"
-                          viewBox="0 0 75 75"
+            {result.phonetics &&
+              result.phonetics.map((phonetic, phoneticIdx) => (
+                <div key={phoneticIdx}>
+                  {phonetic.audio &&
+                    phonetic.audio.trim() !== "" &&
+                    (phoneticIdx === 0 ||
+                      !result.phonetics
+                        .slice(0, phoneticIdx)
+                        .some((p) => p.audio.trim() !== "")) && (
+                      <div>
+                        <audio id={`audio_${idx}_${phoneticIdx}`}>
+                          <source src={phonetic.audio} type="audio/mpeg" />
+                        </audio>
+                        <button
+                          id={`playButton_${idx}_${phoneticIdx}`}
+                          onClick={() => handlePlayClick(idx, phoneticIdx)}
                         >
-                          <g fill="#A445ED" fill-rule="evenodd">
-                            <circle
-                              cx="37.5"
-                              cy="37.5"
-                              r="37.5"
-                              opacity=".25"
-                            />
-                            <path d="M29 27v21l21-10.5z" />
-                          </g>
-                        </svg>
-                      </button>
-                    </div>
-                  )}
-              </div>
-            ))}
-            </div>
-            </div>
-          <div className="results-meanings">
-            {result.meanings &&
-              result.meanings.map((meaning, meaningIdx) => (
-                <div key={meaningIdx}>
-                  <h2>Part of Speech: {meaning.partOfSpeech}</h2>
-                  <ul>
-                    {meaning.definitions.map((definition, defIdx) => (
-                      <li key={defIdx}>
-                        <strong>Definition: {definition.definition}</strong>
-                        {definition.example && (
-                          <div>
-                            <strong>Example: {definition.example}</strong>
-                          </div>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
+                          <svg
+                            className="playSVG"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="75"
+                            height="75"
+                            viewBox="0 0 75 75"
+                          >
+                            <g fill="#A445ED" fill-rule="evenodd">
+                              <circle
+                                cx="37.5"
+                                cy="37.5"
+                                r="37.5"
+                                opacity=".25"
+                              />
+                              <path d="M29 27v21l21-10.5z" />
+                            </g>
+                          </svg>
+                        </button>
+                      </div>
+                    )}
                 </div>
               ))}
           </div>
         </div>
+        <div className="results-meanings">
+          {result.meanings &&
+            result.meanings.map((meaning, meaningIdx) => (
+              <div key={meaningIdx}>
+                <div className="meaning-header-container">
+                <h2>{meaning.partOfSpeech}</h2>
+                <div className="hl"></div>
+                </div>
+                <h3>Meaning</h3>
+                  {meaning.definitions.map((definition, defIdx) => (
+                      <li key={defIdx}>
+                      <ul>{definition.definition}
+                      </ul>
+                      {definition.example && (
+                        <div>
+                          "{definition.example}"
+                        </div>
+                      )}
+                    </li>
+                  ))}
+              </div>
+            ))}
+        </div>
+      </div>
     ));
   } else {
     content = <p>No results found.</p>;
