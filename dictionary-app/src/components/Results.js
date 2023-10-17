1be4a24b5d550dcd85fa3b5378616dc03f60c254
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-10-16 21:08:48 -0400

    Add logic to identify and display the first audio source with a non-null value

--- dictionary-app/src/components/Results.js
+++ dictionary-app/src/components/Results.js
@@ -21,46 +21,63 @@ function Results({ errorMessage, results }) {
           {result.phonetics &&
             result.phonetics.map((phonetic, phoneticIdx) => (
               <div key={phoneticIdx}>
-                <audio id={`audio_${idx}_${phoneticIdx}`}>
-                  <source src={phonetic.audio} type="audio/mpeg" />
-                </audio>
-                <button
-                  id={`playButton_${idx}_${phoneticIdx}`}
-                  onClick={() => handlePlayClick(idx, phoneticIdx)}
-                >
-                  <svg
-                    className="playSVG"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="75"
-                    height="75"
-                    viewBox="0 0 75 75"
-                  >
-                    <g fill="#A445ED" fill-rule="evenodd">
-                      <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
-                      <path d="M29 27v21l21-10.5z" />
-                    </g>
-                  </svg>
-                </button>
+                {/* Check if phonetic.audio is not null and a valid source, and if it's the first valid source */}
+                {phonetic.audio &&
+                  phonetic.audio.trim() !== "" &&
+                  (phoneticIdx === 0 ||
+                    !result.phonetics
+                      .slice(0, phoneticIdx)
+                      .some((p) => p.audio.trim() !== "")) && (
+                    <div>
+                      <audio id={`audio_${idx}_${phoneticIdx}`}>
+                        <source src={phonetic.audio} type="audio/mpeg" />
+                      </audio>
+                      <button
+                        id={`playButton_${idx}_${phoneticIdx}`}
+                        onClick={() => handlePlayClick(idx, phoneticIdx)}
+                      >
+                        <svg
+                          className="playSVG"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="75"
+                          height="75"
+                          viewBox="0 0 75 75"
+                        >
+                          <g fill="#A445ED" fill-rule="evenodd">
+                            <circle
+                              cx="37.5"
+                              cy="37.5"
+                              r="37.5"
+                              opacity=".25"
+                            />
+                            <path d="M29 27v21l21-10.5z" />
+                          </g>
+                        </svg>
+                      </button>
+                    </div>
+                  )}
               </div>
             ))}
-          {result.meanings &&
-            result.meanings.map((meaning, meaningIdx) => (
-              <div key={meaningIdx}>
-                <h2>Part of Speech: {meaning.partOfSpeech}</h2>
-                <ul>
-                  {meaning.definitions.map((definition, defIdx) => (
-                    <li key={defIdx}>
-                      <strong>Definition: {definition.definition}</strong>
-                      {definition.example && (
-                        <div>
-                          <strong>Example: {definition.example}</strong>
-                        </div>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+          <div className="results-meanings">
+            {result.meanings &&
+              result.meanings.map((meaning, meaningIdx) => (
+                <div key={meaningIdx}>
+                  <h2>Part of Speech: {meaning.partOfSpeech}</h2>
+                  <ul>
+                    {meaning.definitions.map((definition, defIdx) => (
+                      <li key={defIdx}>
+                        <strong>Definition: {definition.definition}</strong>
+                        {definition.example && (
+                          <div>
+                            <strong>Example: {definition.example}</strong>
+                          </div>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     ));
